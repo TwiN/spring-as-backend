d59c9e857c6bee07b9d9e2bd7afcdcee565bc243
TwinProduction <christian-ct@hotmail.com>
2018-09-19 16:51:31 -0400

    Removed unused imports

--- src/main/java/org/twinnation/springasbackend/exception/UnauthenticatedException.java
+++ src/main/java/org/twinnation/springasbackend/exception/UnauthenticatedException.java
@@ -1,8 +1,5 @@
 package org.twinnation.springasbackend.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 
 public class UnauthenticatedException extends Exception {
 	
